d273769607f4f9331e21f2884f826c4e84fa4eaa
John Duimovich <jduimovich@gmail.com>
2021-04-14 13:44:18 -0400

    allow local kube linter executable

--- kubelint.sh
+++ kubelint.sh
@@ -3,16 +3,21 @@ then
 echo usage kubelint directory [filter]
 exit
 fi
-export LINT=/dir/$1
+export LINT=$1
 
 if [ "$2" == "" ]
-then
-echo no filter
+then 
 set FILTER=
+else  
+export FILTER="--config $2"
+export DFILTER="--config /dir/$2"
+fi 
+if [ "$3" == "docker" ]
+then
+echo docker run -v $(PWD):/dir stackrox/kube-linter lint /dir/$LINT  $DFILTER
+docker run -v $(PWD):/dir stackrox/kube-linter lint /dir/$LINT  $DFILTER
 else 
-echo filter is $2
-export FILTER="--config /dir/$2"
+echo kube-linter lint $LINT  $FILTER 
+kube-linter lint $LINT  $FILTER 
 fi
-echo kublint $LINT $FILTER
-docker run -v $(PWD):/dir stackrox/kube-linter lint $LINT  $FILTER
 
